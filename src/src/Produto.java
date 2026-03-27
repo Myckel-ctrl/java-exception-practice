23f64f20037ee6e43fc56fbc4a022ee70a012292
Myckel de Vasconcelos Mota <myckel.vasconcelos638@gmail.com>
2026-03-26 21:57:01 -0300

    fix: corrige validação de preço inválido

--- src/src/Produto.java
+++ src/src/Produto.java
@@ -6,6 +6,11 @@ public class Produto {
     private String categoria;
 
     public Produto(String nome, double preco, int quantidade, String categoria) {
+
+        if (preco <= 0) {
+            throw new ValorInvalidoException("Preço inválido" + preco);
+        }
+
         this.nome = nome;
         this.preco = preco;
         this.quantidade = quantidade;
