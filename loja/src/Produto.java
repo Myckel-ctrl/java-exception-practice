2ad53a58958417936807bb00ceb4f4fdc0fae27f
Myckel de Vasconcelos Mota <myckel.vasconcelos638@gmail.com>
2026-03-28 21:03:57 -0300

    chore: recria estrutura do projeto corrigindo configuração

---
+++ loja/src/Produto.java
@@ -0,0 +1,27 @@
+public class Produto {
+
+    private String nome;
+    private double preco;
+    private int quantidade;
+    private String categoria;
+
+    public Produto(String nome, double preco, int quantidade, String categoria) {
+
+        if (preco <= 0) {
+            throw new ValorInvalidoException("Preço inválido " + preco);
+        }
+
+        if (quantidade <= 0) {
+            throw new ValorInvalidoException("Quantidade inválida " + quantidade);
+        }
+
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        this.categoria = categoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
---
+++ loja/src/ProdutoService.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class ProdutoService {
+
+    private ArrayList<Produto> produtos = new ArrayList<>();
+
+    public void cadastrar(Produto produto) {
+        produtos.add(produto);
+        System.out.println("Produto cadastrado com sucesso.");
+    }
+
+    public void remover(String nome) {
+        for(Produto p: produtos) {
+            if (p.getNome().equalsIgnoreCase(nome)){
+                produtos.remove(p);
+                System.out.println("Produto removido com sucesso.");
+                return;
+            }
+        }
+
+        System.out.println("Produto não encontrado.");
+    }
+
+    public void listar() {
+        if (produtos.isEmpty()) {
+            System.out.println("Nenhum produto cadastrado.");
+            return;
+        }
+
+        for(Produto p: produtos) {
+            System.out.println(p.getNome());
+        }
+    }
+
+    public void buscar(String nome) {
+        for(Produto p: produtos) {
+            if (p.getNome().equalsIgnoreCase(nome)){
+                System.out.println(p.getNome());
+                return;
+            }
+        }
+
+        System.out.println("Nenhum produto encontrado.");
+    }
+}
